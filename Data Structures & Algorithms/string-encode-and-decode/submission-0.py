657855b9b57c9c26dc2d00b618f8d6884459d5a6
Rajoshi Basu <34625067+R1A2J@users.noreply.github.com>
2026-05-23 14:00:13 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        s, l = '', ''
+        for i in strs:
+            s += i + '\u0986'
+        '''201 + 402 + ... + 200(201) = 
+        '''
+        return s
+
+    def decode(self, s: str) -> List[str]:
+        answer = []
+        start = 0
+        for i in range(len(s)):
+            if s[i] == '\u0986':
+                answer.append(s[start:i])
+                start = i + 1
+        return answer
